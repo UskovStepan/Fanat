6fd880b8e38b32da184ac07edef3345657c13b8e
WorkStepan <ustepan94@gmail.com>
2025-05-13 16:47:22 +0700

    Заново начал переписывать CSS

--- fanat_tomsk/fanat/urls.py
+++ fanat_tomsk/fanat/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('price/', views.price, name='price'),
     path('shop/', views.shop, name='shop'),
 	path('login/', views.login, name='login')	
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
